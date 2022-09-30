2d78de4aac86c418947e63cd95cda9cb9f082df8
litianyi <litianyi@bolegames.com>
2022-09-30 18:15:22 +0800

    email init

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -28,3 +30,25 @@ def post_deatil(req, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
     return render(req, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(req, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status='published')
+    if req.method == 'Post':
+        # form was submitted
+        form = EmailPostForm(req.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(req, 'blog/post/share.html', {'post': post, 'form': form})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
